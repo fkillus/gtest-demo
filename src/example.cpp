7d23c1adb3393074253ce0bd61a3bf125c937f95
fkillus <fkillus@users.noreply.github.com>
2017-07-17 19:51:27 +0200

    Update example.cpp

--- src/example.cpp
+++ src/example.cpp
@@ -5,12 +5,12 @@ double add_numbers(const double f1, const double f2)
     return f1 + f2;
 }
 
-double subtract_numbers(const double f1, const double f2)
+double subtract_mumbers(const double f1, const double f2)
 {
-    return f1 + f2;
+    return f1 - f2;
 }
 
-double multiply_mumbers(const double f1, const double f2)
+double multiply_numbers(const double f1, const double f2)
 {
     return f1*f2;
 }
